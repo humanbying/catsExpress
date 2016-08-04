2ba75da28a9e5090679418c45b6e7ff978525b9e
Brian Ying <humanbying@gmail.com>
2016-08-04 10:37:17 -0700

    ready for heroku

---
+++ app.js
@@ -0,0 +1,82 @@
+'use strict';
+console.log('process.end.PWD', process.env.PWD);
+const PORT = process.env.PORT || 8000;
+
+const express = require('express'),
+request = require('request');
+
+const bodyParser = require('body-parser');
+const Cat = require('./models/cat');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+//location parameter and anonoymous function
+app.get('/', function(req, res) {
+  res.send("<h1>Pink Panther</h1>");
+});
+
+
+app.route('/cats')
+.get((req, res) => {
+  // Get all cats
+  Cat.getAll(function(err, cats) {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      res.send(cats);
+    }
+  });
+})
+.post((req, res) => {
+  console.log("from post");
+  Cat.create(req.body, function(err) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send('cat created!\n');
+    }
+  })
+});
+
+
+app.route('/cats/:id')
+.get((req, res) => {
+  Cat.getOne(req.params.id, function(err, catObj){
+    if(err){
+      res.status(400).send(err);
+    } else {
+      res.send(catObj);
+    }
+  })
+  // res.send(`Here is cat #${req.params.id}! \n`)
+})
+.put((req, res) => {
+  Cat.modifyOne(req.params.id, function(err, catObj){
+    if(err){
+      res.status(400).send(err);
+    } else {
+      res.send(catObj);
+    }
+  })
+  // res.send(`Editing cat #${req.params.id}!`)
+})
+.delete((req, res) => {
+  Cat.deleteOne(req.params.id, function(err, catObj){
+    if(err){
+      res.status(400).send(err);
+    } else {
+      res.send(catObj);
+    }
+  })
+  // res.send(`Delete cat #${req.params.id}!`)
+});
+
+app.listen(`${PORT}`, function() {
+  console.log(`The server is running on port ${PORT}`);
+});
---
+++ models/cat.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+
+
+const dataFilePath = path.join(__dirname,'./cats.json');
+
+
+exports.getOne = function(id, cb) {
+  exports.getAll(function(err, cats){
+    // console.log(cats);
+    if(err) return cb(err);
+    const chosenCat = cats.find(function(cat){
+      if(id === cat.id) return true;
+      else return false;
+    })
+    return cb(null, chosenCat);
+  });
+}
+
+// exports.modifyOne = function(id, cb) {
+//   exports.getAll(function(err, cats){
+//     // console.log(cats);
+//     if(err) return cb(err);
+//
+//     let modifiedCat = cats.find(function(cat){
+//       if(id === cat.id) return true;
+//       else return false;
+//     // modifiedCat = x
+//
+//     // cats.push()
+//     })
+//     return cb(null, chosenCat);
+//   });
+//
+//     // return cb(null, cats);
+//   });
+// });
+// }
+
+
+exports.deleteOne = function(id, cb) {
+  exports.getAll(function(err, cats){
+    // console.log(cats);
+    if(err) return cb(err);
+    let index = cats.findIndex(cats => cats.id === id);
+    cats.splice(index, 1);
+    console.log(cats);
+    fs.writeFile(dataFilePath, JSON.stringify(cats), function(err) {
+      cb(err);
+    // return cb(null, cats);
+  });
+});
+}
+
+
+exports.getAll = function(cb) {
+
+  fs.readFile(dataFilePath, (err, buffer) => {
+    if(err) {
+      return cb(err);
+    }
+    let cats;
+
+    try {
+      cats = JSON.parse(buffer);
+    } catch(err) {
+      return cb(err);
+    }
+
+    return cb(null, cats);
+  });
+}
+
+exports.create = function(catObj, cb) {
+  console.log(" from cat.js")
+  exports.getAll(function(err, cats) {
+    if(err) return cb(err);
+
+    catObj.id = uuid.v4();
+
+    cats.push(catObj);
+
+    fs.writeFile(dataFilePath, JSON.stringify(cats), function(err) {
+      cb(err);
+    });
+  });
+}
